21b38e28e3db35370e16381947df1b34d372196d
Thane <thabay0606@students.maquoketaschools.org>
2026-03-02 13:55:01 -0600

    Maybe?

--- Registration.cs
+++ Registration.cs
@@ -3,6 +3,13 @@ public class Registration
 {
     // Add your code here
     public int ValidateAge(bool oldEnough){
-        oldEnough = 
+        
+        if (oldEnough == true){
+            return "old enough";
+        }
+        else{
+            return "";
+        }
+
     }
 }
